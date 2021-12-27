cc0553493236aa46ebe5b12f04348c3cf344c6d7
Serhii <jshmitz@me.com>
2021-12-27 14:25:50 +0000

    translate to UA

--- src/htdocs/svg/screen_list.js
+++ src/htdocs/svg/screen_list.js
@@ -4,16 +4,16 @@
 
 optionhtml = "\
 <option id='SELTELA_OPC1' selected disabled='disabled'>Choose a screen ...</option>\
-<optgroup label='Substation 1'>\
+<optgroup label='Підстанція 1'>\
   <option value='../svg/kaw2.svg' >KAW2 ............[1]{A}</option>\
 </optgroup>\
-<optgroup label='Substation 2'>\
+<optgroup label='Підстанція 2'>\
   <option value='../svg/kor1.svg' >KOR1.............[2]{O}</option>\
 </optgroup>\
-<optgroup label='Substation 3'>\
+<optgroup label='Підстанція 3'>\
   <option value='../svg/knh2.svg' >KNH2.............[3]{N}</option>\
 </optgroup>\
-<optgroup label='Substation 4'>\
+<optgroup label='Підстанція 4'>\
   <option value='../svg/kik3.svg' >KIK3.............[4]{I}</option>\
 </optgroup>\
 ";
